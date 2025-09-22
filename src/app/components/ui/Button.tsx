8fb0ba07bd36aa272e182e4989f917b705313675
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-22 17:18:37 +0330

    update size

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -28,9 +28,9 @@ const Button: React.FC<ButtonProps> = ({
   };
 
   const sizeClass = {
-    xs: "px-2 py-2 text-sm",
-    md: "px-4 py-3 text-base",
-    lg: "px-6 py-4 text-lg",
+    xs: "px-2 py-1 text-sm",
+    md: "px-3 py-2 text-base",
+    lg: "px-4 py-3 text-lg",
   };
   return (
     <button
