e6817324235ceb002889ad9e09ebf8bdfe632f81
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-22 13:26:48 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import AddTask from "./components/AddTask";
-import AddLead from "./components/lead/AddLead";
-
 export default function Home() {
   return (
     <div>
       <main>
         <h1 className="text-red-600">به پشتیبانی خوش آمدید</h1>
-        <AddTask />
-        <AddLead/>
       </main>
     </div>
   );
