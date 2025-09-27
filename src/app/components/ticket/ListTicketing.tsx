e2cc3b05e7d084902e1c643238ac17da917c641f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-27 15:35:15 +0330

    add nothing ticket

--- src/app/components/ticket/ListTicketing.tsx
+++ src/app/components/ticket/ListTicketing.tsx
@@ -12,7 +12,42 @@ const ListTicketing:React.FC<ListTicketTableProps> = ({tickets, onEdit, onDelete
   return (
     <div className='mt-4'>
       <p className='font-bold text-white mb-2'>تعداد تیکتهای ثبت شده{countTickets}</p>
-    </div>
+      <div>
+       <div className="overflow-x-auto border rounded-md">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                #
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                موضوع
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                شرح
+              </th>
+              <th className="px-4 py-2 text-center text-sm font-medium text-gray-700">
+                عملیات
+              </th>
+            </tr>
+          </thead>
+          <tbody className='bg-white divide-y divide-gray-100'>
+            {tickets.length === 0 &&(
+              <tr>
+                 <td
+                  colSpan={4}
+                  className="px-4 py-6 text-center text-sm text-gray-500"
+                >
+                  <p>هیچ تیکتی ثبت نشده </p>
+                </td>
+              </tr>
+            )}
+
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
   )
 }
 
