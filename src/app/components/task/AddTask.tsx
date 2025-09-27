4187df43668dab6d5d31ac2f0fb95f02b399c624
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-27 13:09:39 +0330

    add

--- src/app/components/task/AddTask.tsx
+++ src/app/components/task/AddTask.tsx
@@ -25,6 +25,18 @@ const AddTask = () => {
     setInputValue("");
   }, [inputValue]);
 
+  const handleEdit = useCallback((id: Task["id"], newTask: string) => {
+    setTasks((prev) =>
+      prev.map((t) =>
+        String(t.id) === String(id) ? { ...t, text: newTask } : t,
+      ),
+    );
+  }, []);
+
+  const handleDelete = useCallback ((id: Task["id"])=>{
+    setTasks((prev)=>prev.filter((t)=>String(t.id)!== String(id)))
+  },[])
+
   return (
     <div className="md:mx-auto max-w-4xl bg-gradient-to-br from-slate-900 to-slate-950 shadow rounded-xl p-6 md:p-8 mr-2 ml-2 mt-5">
       <h2 className="font-bold text-white text-md md:text-xl mb-6">
@@ -41,7 +53,7 @@ const AddTask = () => {
         <div className="flex gap-2 items-center mb-4">
           <Button onClick={handleAddTask}>اضافه</Button>
         </div>
-        <ListTaskTable tasks={tasks} />
+        <ListTaskTable tasks={tasks} onEdit={handleEdit} onDelete={handleDelete}/>
       </div>
     </div>
   );
