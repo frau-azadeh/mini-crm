7f10f98f82ac9f50773f8d26a56ff6ef36864cb1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-27 12:14:33 +0330

    update prettier

--- src/app/components/task/AddTask.tsx
+++ src/app/components/task/AddTask.tsx
@@ -1,53 +1,50 @@
-"use client"
-import React, { useCallback, useMemo, useState } from 'react'
-import { Task } from '@/types/types'
-import Button from '../ui/Button'
-import ListTaskTable from './ListTaskTable'
+"use client";
+
+import React, { useCallback, useMemo, useState } from "react";
+
+import { Task } from "@/types/types";
+
+import Button from "../ui/Button";
+import ListTaskTable from "./ListTaskTable";
+
 const AddTask = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
 
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [inputValue, setInputValue] = useState<string>("")
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }, []);
 
-  const handleChange = useCallback((e:React.ChangeEvent<HTMLInputElement>)=>{
-    setInputValue(e.target.value)
-  },[])
-
-  const   handleAddTask = useCallback(()=>{
-    if(!inputValue.trim()) return
-    const newTask: Task ={
+  const handleAddTask = useCallback(() => {
+    if (!inputValue.trim()) return;
+    const newTask: Task = {
       id: Date.now().toString(),
-      text: inputValue.trim()
-    }
-    setTasks((prev)=> [...prev, newTask])
-    setInputValue("")
-  },[inputValue])
-
+      text: inputValue.trim(),
+    };
+    setTasks((prev) => [...prev, newTask]);
+    setInputValue("");
+  }, [inputValue]);
 
   return (
     <div className="md:mx-auto max-w-4xl bg-gradient-to-br from-slate-900 to-slate-950 shadow rounded-xl p-6 md:p-8 mr-2 ml-2 mt-5">
-      
-      <h2 className='font-bold text-white text-md md:text-xl mb-6'>
+      <h2 className="font-bold text-white text-md md:text-xl mb-6">
         تسک خود را اضافه کنید!
       </h2>
       <div className="bg-white rounded-lg p-6 shadow-inner mb-6">
-        <span className="block text-sm text-gray-600 mb-4">
-          تسک جدید: 
-          </span>
+        <span className="block text-sm text-gray-600 mb-4">تسک جدید:</span>
         <input
           value={inputValue}
           onChange={handleChange}
-          placeholder='تسک جدید ...'
+          placeholder="تسک جدید ..."
           className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
         />
-        <div className='flex gap-2 items-center mb-4'>
-          <Button
-            onClick={handleAddTask}
-          >اضافه</Button>
+        <div className="flex gap-2 items-center mb-4">
+          <Button onClick={handleAddTask}>اضافه</Button>
         </div>
-<ListTaskTable tasks={tasks}/>
+        <ListTaskTable tasks={tasks} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddTask
+export default AddTask;
