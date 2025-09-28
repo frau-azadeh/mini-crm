0bbe121efbfcf1c570648019ae00bce9620c286a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-28 14:21:43 +0330

    add map list

--- src/app/components/Branch/ListBranch.tsx
+++ src/app/components/Branch/ListBranch.tsx
@@ -1,5 +1,6 @@
 import { Branch } from '@/types/types'
 import React, { useMemo } from 'react'
+import Button from '../ui/Button'
 
 interface ListBranchTableProps{
     branches :Branch[],
@@ -11,6 +12,65 @@ const ListBranch:React.FC<ListBranchTableProps> = ({branches}) => {
   return (
     <div className='mt-4'>
         <p className='text-white'>تعداد شعب ثبت شده{countBranches}</p>
+        <div className='overflow-x-auto border rounded-md'>
+          <table className='min-w-full divide-y divide-gray-200'>
+            <thead className='bg-gray-50'>
+              <tr>
+<th  className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                #
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                نام شعب
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                نشانی
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                شماره تلفن
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+               وضعیت
+              </th>
+              <th className="px-4 py-2 text-center text-sm font-medium text-gray-700">
+                عملیات
+              </th>
+              </tr>
+            </thead>
+            <tbody className='bg-white divide-y divide-gray-100'>
+               {branches.length === 0&&(
+              <tr>
+               
+                 
+                      <td
+                  colSpan={6}
+                  className="px-4 py-6 text-center text-sm text-gray-500"
+                >
+                      <p>هیچ شعبه ای وجود ندارد</p>
+                    </td>
+                 
+               
+              </tr>
+               )}
+               {branches.map((branch, index)=>(
+                <tr key={branch.id}>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">{index+1}</td>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">{branch.city}</td>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">{branch.address}</td>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">{branch.phone}</td>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">{branch.status}</td>
+                                 <td className="px-4 py-3 text-right text-sm text-gray-700 ">
+                                  <div className='flex gap-2 items-center justify-center'>
+                                    <Button variant='call'>ویرایش</Button>
+                                    <Button variant='danger'>حذف</Button>
+                                  </div>
+                                 </td>
+
+                </tr>
+               ))}
+            </tbody>
+          </table>
+
+        </div>
     </div>
   )
 }
