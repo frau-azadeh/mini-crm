9a0ac1196c991c49b24faced9050fd9f18d5c077
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-28 12:53:41 +0330

    update prettier

--- src/app/components/Branch/AddBranch.tsx
+++ src/app/components/Branch/AddBranch.tsx
@@ -1,48 +1,51 @@
-"use client"
+"use client";
 
-import { Branch } from '@/types/types'
-import React, { useCallback, useState} from 'react'
-import Button from '../ui/Button'
+import React, { useCallback, useState } from "react";
 
-const AddBranch:React.FC = () => {
+import { Branch } from "@/types/types";
 
-  const[branchs, setBranchs] = useState<Branch[]>([])
-  const[form, setForm] = useState<Omit<Branch, "id">>({
-    city:"",
-    phone:"",
-    address:"",
-    status:""
-  })
+import Button from "../ui/Button";
 
-  const handleChange = useCallback((e:React.ChangeEvent<HTMLInputElement>)=>{
-    const {name, value} = e.target
-    setForm((prev)=>({...prev,[name]:value}))
-  },[])
+const AddBranch: React.FC = () => {
+  const [branchs, setBranchs] = useState<Branch[]>([]);
+  const [form, setForm] = useState<Omit<Branch, "id">>({
+    city: "",
+    phone: "",
+    address: "",
+    status: "",
+  });
 
-  const handleAdd = useCallback(()=>{
-    if(!form.city.trim()) return
-    
-    const newBranch: Branch={
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    if (!form.city.trim()) return;
+
+    const newBranch: Branch = {
       id: Date.now().toString(),
-      ...form
-    }
-    setBranchs((prev)=>[...prev, newBranch])
+      ...form,
+    };
+    setBranchs((prev) => [...prev, newBranch]);
     setForm({
-      city:"",
-      phone:"",
-      address:"",
-      status:""
-    })
-  },[form])
+      city: "",
+      phone: "",
+      address: "",
+      status: "",
+    });
+  }, [form]);
 
   return (
-    <div className='mx-auto max-w-4xl bg-gradient-to-br from-slate-900 to-slate-950 shadow-lg rounded-xl md:p-8'>
-      <h2 className='font-bold text-white text-xl md:text-2xl mb-6'>اطلاعات شعب را وارد کنید!</h2>
-      <div className='bg-white rounded-lg p-6 shadow-inner'>
+    <div className="mx-auto max-w-4xl bg-gradient-to-br from-slate-900 to-slate-950 shadow-lg rounded-xl md:p-8">
+      <h2 className="font-bold text-white text-xl md:text-2xl mb-6">
+        اطلاعات شعب را وارد کنید!
+      </h2>
+      <div className="bg-white rounded-lg p-6 shadow-inner">
         <input
-          name='city'
+          name="city"
           value={form.city}
-          placeholder='نام شهر'
+          placeholder="نام شهر"
           onChange={handleChange}
           className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
         />
@@ -50,31 +53,31 @@ const AddBranch:React.FC = () => {
         <input
           name="address"
           value={form.address}
-          placeholder='نشانی'
+          placeholder="نشانی"
           onChange={handleChange}
-          className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"       
-          />
-
-        <input
-          name='phone'
-          value={form.phone}
-          onChange={handleChange}
-          placeholder='شماره همراه'
-          className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"       
+          className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
         />
 
-          <input
-            name='status'
-            value={form.status}
-            placeholder='وضعیت'
-            onChange={handleChange}
-            className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
-          />
+        <input
+          name="phone"
+          value={form.phone}
+          onChange={handleChange}
+          placeholder="شماره همراه"
+          className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
+        />
 
-          <Button onClick={handleAdd}>ذخیره</Button>
+        <input
+          name="status"
+          value={form.status}
+          placeholder="وضعیت"
+          onChange={handleChange}
+          className="border border-gray-300 rounded-lg p-2 mb-3 w-full focus:ring-2 focus:ring-indigo-400 focus:outline-none transition"
+        />
+
+        <Button onClick={handleAdd}>ذخیره</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddBranch
+export default AddBranch;
