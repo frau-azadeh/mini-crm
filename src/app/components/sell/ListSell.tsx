c9f5e30551dfc22d11f8bb68030e193e110df5e3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-28 21:27:35 +0330

    update prettier

--- src/app/components/sell/ListSell.tsx
+++ src/app/components/sell/ListSell.tsx
@@ -1,16 +1,39 @@
-import { Sell } from "@/types/types";
 import React, { useMemo } from "react";
 
-interface ListSellTableProps{
-    sells: Sell[]
-}
-const ListSell:React.FC<ListSellTableProps> = ({sells}) => {
+import { Sell } from "@/types/types";
 
-    const countSell = useMemo(()=> sells.length,[sells])
+interface ListSellTableProps {
+  sells: Sell[];
+}
+const ListSell: React.FC<ListSellTableProps> = ({ sells }) => {
+  const countSell = useMemo(() => sells.length, [sells]);
 
   return (
-    <div>
-       <p className="font-bold text-white mb-2">تعداد محصولات ثبت شده{countSell}</p>
+    <div className="mt-4">
+      <p className="font-bold text-white mb-2">
+        تعداد محصولات ثبت شده{countSell}
+      </p>
+
+      <div className="overflow-x-auto border rounded-md">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                #
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                موضوع
+              </th>
+              <th className="px-4 py-2 text-right text-sm font-medium text-gray-700">
+                شرح
+              </th>
+              <th className="px-4 py-2 text-center text-sm font-medium text-gray-700">
+                عملیات
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 };
