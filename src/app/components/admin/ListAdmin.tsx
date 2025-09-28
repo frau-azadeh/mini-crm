26fe937a4be277b9d5bf6cc0aed0ba94f71305fe
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-28 11:07:50 +0330

    add delete props

--- src/app/components/admin/ListAdmin.tsx
+++ src/app/components/admin/ListAdmin.tsx
@@ -1,18 +1,28 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 import { Admin } from "@/types/types";
+import Button from "../ui/Button";
 
 interface ListAdminTableProps {
-  admin: Admin[];
+  admins: Admin[];
   onDelete?: (id: Admin["id"]) => void;
   onEdit?: (id: Admin["id"], newData: Omit<Admin, "id">) => void;
 }
 const ListAdmin: React.FC<ListAdminTableProps> = ({
-  admin,
+  admins,
   onEdit,
   onDelete,
 }) => {
-  const countAdmin = useMemo(() => admin.length, [admin]);
+
+    const [editingId, setEditingId] = useState <Admin["id"] | null>(null)
+    const [editData, setEditData] = useState<Omit<Admin, "id">|null>(null)
+  const countAdmin = useMemo(() => admins.length, [admins]);
+
+
+  const handleDelete = useCallback((id: Admin["id"])=>{
+    onDelete?.(id)
+  },[onDelete])
+
   return (
     <div className="mt-4">
       <p className="font-bold text-white mb-2">
@@ -42,6 +52,33 @@ const ListAdmin: React.FC<ListAdminTableProps> = ({
               </th>
             </tr>
           </thead>
+          <tbody className="bg-white divide-y divide-gray-100">
+            {admins.length === 0 &&(
+                <tr>
+                    <td colSpan={6} 
+                    className="px-4 py-6 text-center text-sm text-gray-500"
+                    >
+                        <p>هیچ ادمینی ثبت نشده است!</p>
+                    </td>
+                </tr>
+            )}
+            {admins.map((admin, index)=>(
+                <tr key={admin.id}>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700 ">{index+1}</td>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700">{admin.name}</td>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700 ">{admin.family}</td>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700 ">{admin.userName}</td>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700 ">{admin.password}</td>
+                    <td className="px-4 py-3 text-right text-sm text-gray-700 ">
+                        <div className="flex items-center gap-2 justify-center">
+                            <Button variant="call">ویرایش</Button>
+                            <Button variant="danger" onClick={()=>handleDelete(admin.id)}>حذف</Button>
+                        </div>
+                    </td>
+                </tr>
+            ))}
+            
+          </tbody>
         </table>
       </div>
     </div>
