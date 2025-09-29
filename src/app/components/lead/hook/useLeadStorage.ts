4f90e435c9b5c6f56fd27844ecb09ac126c6d2b2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-29 15:35:20 +0330

    add leads

--- src/app/components/lead/hook/useLeadStorage.ts
+++ src/app/components/lead/hook/useLeadStorage.ts
@@ -1,3 +1,30 @@
-export function useLeadStorage(){
-    return
+import { useEffect, useState } from "react";
+
+import { Lead } from "@/types/types";
+
+const STORAGE_KEY = "my_app_lead";
+
+export function useLeadStorage() {
+  const [leads, setLeads] = useState<Lead[]>([]);
+
+  useEffect(() => {
+    try {
+        const raw = localStorage.getItem(STORAGE_KEY)
+        if(!raw) return
+        const parsed = JSON.parse(raw) as Lead[] | null
+        if(Array.isArray(parsed)) setLeads(parsed)
+    } catch (error) {
+      console.error("خطا در خواندن در localStorage", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(leads))
+    } catch (error) {
+      console.error("خطا در نوشتن در localStorage", error);
+    }
+  }, [leads]);
+
+  return { leads };
 }
