231313a4192b2a8f5fe964bc78b7fe2f46033f87
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-29 19:23:02 +0330

    add useTicketingStorage

---
+++ src/app/components/ticket/hook/useTicketingStorage.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+import { Ticket } from "@/types/types";
+
+const STORAGE_KEY = "my_app_ticketing";
+
+export function useTicketingStorage() {
+  const [ticketing, setTicketing] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    try {
+        const raw = localStorage.getItem(STORAGE_KEY)
+        if(!raw) return
+        const parsed = JSON.parse(raw) as Ticket[]| null
+        if(Array.isArray(parsed)) setTicketing(parsed)
+    } catch (error) {
+        console.error("خطا در خواندن از localStorage",error)
+    }
+  });
+
+  useEffect(() => {
+    try {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(ticketing))
+    } catch (error) {
+        console.error("خطا از نوشتن در localStorage",error)
+    }
+  }, [ticketing]);
+
+  return { ticketing };
+}
