164b1fa1400d7969072eb4a863dd9fdb395b801b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-29 17:24:40 +0330

    add read and write in localStorage

--- src/app/components/task/hook/useTaskStorage.ts
+++ src/app/components/task/hook/useTaskStorage.ts
@@ -1,10 +1,33 @@
-import { Task } from "@/types/types"
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
-const STORAGE_KEY = "my_app_task"
+import { json } from "stream/consumers";
 
-export function useTaskStorage(){
+import { Task } from "@/types/types";
 
-    const [tasks, setTask] = useState<Task[]>([])
-    return
+const STORAGE_KEY = "my_app_task";
+
+export function useTaskStorage() {
+  const [tasks, setTask] = useState<Task[]>([]);
+
+  useEffect(() => {
+    try {
+      const raw = localStorage.getItem(STORAGE_KEY);
+      if (!raw) return;
+      const parsed = JSON.parse(raw) as Task[] | null;
+      if (Array.isArray(parsed)) setTask(parsed);
+    } catch (error) {
+      console.error("خطا در خواندن از  localStorage", error);
+    }
+  },[]);
+
+
+  useEffect(()=>{
+    try {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+    } catch (error) {
+        console.error("خطا در نوشتن در localStorage",error)
+    }
+  },[])
+
+  return{tasks}
 }
