ae8bb71024145c8e7d3e10da6aa7d0e803c9a15d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-29 17:15:27 +0330

    add state

--- src/app/components/task/hook/useTaskStorage.ts
+++ src/app/components/task/hook/useTaskStorage.ts
@@ -1,3 +1,10 @@
+import { Task } from "@/types/types"
+import { useState } from "react"
+
+const STORAGE_KEY = "my_app_task"
+
 export function useTaskStorage(){
+
+    const [tasks, setTask] = useState<Task[]>([])
     return
 }
