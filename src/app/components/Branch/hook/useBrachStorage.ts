76fb61f3588b9b6d04817bd234cdf7dfc5d84a54
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-29 15:35:07 +0330

    update prettier

--- src/app/components/Branch/hook/useBrachStorage.ts
+++ src/app/components/Branch/hook/useBrachStorage.ts
@@ -1,43 +1,40 @@
-import { Branch } from "@/types/types"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 
-const STORAGE_KEY = "my_app_branches"
+import { Branch } from "@/types/types";
 
-export function useBranchStorage(){
-    const [branches, setBranches] = useState<Branch[]>([])
-    
+const STORAGE_KEY = "my_app_branches";
 
-    useEffect(()=>{
-        try {
-            const row = localStorage.getItem(STORAGE_KEY);
-            if(!row) return;
-            const parsed = JSON.parse(row) as Branch[] | null;
-            if (Array.isArray(parsed)) setBranches(parsed);
-            
-        } catch (error) {
-            console.error("خطا خواندن در locakStorage", error)
-        }
+export function useBranchStorage() {
+  const [branches, setBranches] = useState<Branch[]>([]);
 
-    },[])
+  useEffect(() => {
+    try {
+      const row = localStorage.getItem(STORAGE_KEY);
+      if (!row) return;
+      const parsed = JSON.parse(row) as Branch[] | null;
+      if (Array.isArray(parsed)) setBranches(parsed);
+    } catch (error) {
+      console.error("خطا خواندن در locakStorage", error);
+    }
+  }, []);
 
-    useEffect(()=>{
-        try {
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(branches))
-        } catch (error) {
-            console.error("خطا در نوشتن در localStorage", error)
-        }
+  useEffect(() => {
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(branches));
+    } catch (error) {
+      console.error("خطا در نوشتن در localStorage", error);
+    }
+  }, [branches]);
 
-    },[branches])
+  const addBranch = useCallback((newBranch: Branch) => {
+    setBranches((prev) => [newBranch, ...prev]);
+  }, []);
 
-    const addBranch = useCallback((newBranch: Branch)=>{
-setBranches((prev)=>[newBranch, ...prev])
-    },[])
+  const deleteBranch = useCallback((id: Branch["id"]) => {
+    setBranches((prev) => prev.filter((t) => String(t.id) !== String(id)));
+  }, []);
 
-    const deleteBranch = useCallback((id: Branch["id"])=>{
-        setBranches((prev)=>prev.filter((t)=>String(t.id)!==String(id)))
-    },[])
-
-      const editBranch = useCallback(
+  const editBranch = useCallback(
     (id: Branch["id"], newData: Omit<Branch, "id">) => {
       setBranches((prev) =>
         prev.map((t) =>
@@ -47,5 +44,5 @@ setBranches((prev)=>[newBranch, ...prev])
     },
     [],
   );
-    return{branches, addBranch, deleteBranch, editBranch}
+  return { branches, addBranch, deleteBranch, editBranch };
 }
