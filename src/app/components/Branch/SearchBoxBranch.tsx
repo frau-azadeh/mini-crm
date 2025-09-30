cd822e438e4993e99bc7850345fa0b8e6ab6b162
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-30 19:17:14 +0330

    done type

--- src/app/components/Branch/SearchBoxBranch.tsx
+++ src/app/components/Branch/SearchBoxBranch.tsx
@@ -1,10 +1,22 @@
 import { Branch } from '@/types/types'
 import React from 'react'
 
-interface BranchesSearchProps{
+type BranchesSearchProps = {
     items: Branch[]
+    value: string
+    onChange: (v: string) => void
+    placeholder?: string
+    debounceMs?: number
+    field?: Array <keyof Pick<Branch, "city" | "status">>
 }
-const SearchBoxBranch:React.FC<BranchesSearchProps> = ({items}) => {
+const SearchBoxBranch:React.FC<BranchesSearchProps> = ({
+    items,
+    value,
+    onChange,
+    placeholder = "جستجو ...",
+    debounceMs = 200,
+    field = ["city", "status"]
+}) => {
   return (
     <div>SearchBoxBranch</div>
   )
