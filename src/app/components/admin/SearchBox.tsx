5d66a248ab5f37bb8b660b962203bfc00cd9b5c1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-30 11:05:59 +0330

    update prettier

--- src/app/components/admin/SearchBox.tsx
+++ src/app/components/admin/SearchBox.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const SearchBox = () => {
-  return (
-    <div>SearchBox</div>
-  )
-}
+  return <div>SearchBox</div>;
+};
 
-export default SearchBox
+export default SearchBox;
