167ece341fbc67e0b44afdb228ff9d2a1c437447
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-04 11:07:00 +0330

    add type

--- src/app/components/Branch/SearchBoxBranch.tsx
+++ src/app/components/Branch/SearchBoxBranch.tsx
@@ -1,56 +1,27 @@
 import { Branch } from '@/types/types'
-import React, { useCallback, useMemo, useRef, useState } from 'react'
+import React from 'react'
 
-type BranchesSearchProps = {
+interface BranchSearchProps{
     items: Branch[]
-    value: string
-    onChange: (v: string) => void
-    placeholder?: string
+    value: string;
+    onChange: (v: string) => void;
+    fields?: Array<keyof Pick<Branch, "city" | "status">>
+    placeholder?: string;
     debounceMs?: number
-    fields?: Array <keyof Pick<Branch, "city" | "status">>
 }
-const SearchBoxBranch:React.FC<BranchesSearchProps> = ({
-    items,
+
+const SearchBoxBranch:React.FC<BranchSearchProps> = ({
     value,
     onChange,
-    placeholder = "جستجو ...",
-    debounceMs = 200,
-    fields = ["city", "status"]
+    items,
+    fields = ["city", "status"],
+    placeholder = "جستجو ... ",
+    debounceMs = 200
 }) => {
 
-    const rootRef = useRef<HTMLDivElement | null>(null)
-    const inputRef = useRef<HTMLInputElement | null>(null)
-    const timerRef = useRef<ReturnType<typeof setTimeout>>(null)
-    const lastSentRef = useRef<string | null>(null)
-
-    const [local, setLocal] = useState<string>(value)
-    const [suggests, setSuggests] = useState <string[]>([])
-    const [open, setOpen] = useState<boolean>(false)
-    const [active, setActive] = useState<number>(-1)
-
-    const flatValues = useMemo (()=>{
-        return items
-            .flatMap(
-                (it)=>
-                     fields
-                     .map((f) => it[f])
-                    .filter((v) => v !== undefined && v!== null)
-                    .map(String)
-            )
-            .filter((v, i, arr)=> arr.indexOf(v)=== i)
-    }, [items, fields])
-
-    const computeSuggestions = useCallback((q: string)=>{
-        const t = q.trim().toLowerCase();
-        if(!t) return[]
-        return flatValues.filter((s)=> s.toLowerCase().includes(t)).slice(0,10)    
     
-    },[flatValues])
-
   return (
-    <div ref={rootRef}>
-        <input ref={inputRef}/>
-    </div>
+    <div>SearchBoxBranch</div>
   )
 }
 
