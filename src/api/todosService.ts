ab84685317c3ec9f8133b228e1294280ef9184cc
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-07 13:36:05 +0330

    update prettier

--- src/api/todosService.ts
+++ src/api/todosService.ts
@@ -1,13 +1,13 @@
 import { apiTodosClient } from "./apiTodosClient";
 
 export type Todos = {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean,
-}
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchAllTodo(): Promise<Todos[]> {
-    const response = await apiTodosClient.get<Todos[]>("/todos")
-    return response.data
+  const response = await apiTodosClient.get<Todos[]>("/todos");
+  return response.data;
 }
