4e52d0e221c53a3a586c25a8efabcab8a5167925
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-09 10:06:57 +0330

    update prettier

--- src/api/commentsService.ts
+++ src/api/commentsService.ts
@@ -1,7 +1,8 @@
 import { Comments } from "@/types/types";
+
 import { apiCommentClient } from "./apiCommentsClient";
 
 export async function fetchAllComments(): Promise<Comments[]> {
-    const response =await apiCommentClient.get<Comments[]>("/comments")
-    return response.data
+  const response = await apiCommentClient.get<Comments[]>("/comments");
+  return response.data;
 }
