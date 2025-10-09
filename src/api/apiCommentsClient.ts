a9cca9e4e2e5f1f9181adffbf5d0b691167b08b7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-09 10:06:47 +0330

    update prettier

--- src/api/apiCommentsClient.ts
+++ src/api/apiCommentsClient.ts
@@ -1,12 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_COMMENT = "https://jsonplaceholder.typicode.com/"
+const API_COMMENT = "https://jsonplaceholder.typicode.com/";
 
 export const apiCommentClient = axios.create({
-    baseURL: API_COMMENT,
-    timeout: 15000,
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
-
+  baseURL: API_COMMENT,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
