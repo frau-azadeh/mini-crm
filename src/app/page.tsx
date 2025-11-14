e5c2eecf823f4abc19e2b70dad5f63ac052368c2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-11-14 23:05:05 +0330

    نمایش در وسط صفحه

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center flex-col min-h-screen">
           <h1 className="text-red-600">به پشتیبانی خوش آمدید</h1>
           <ClassButton>ورود</ClassButton>
         </div>
