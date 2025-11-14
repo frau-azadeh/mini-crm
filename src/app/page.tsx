d72e88282698dea45519a5e6c31a43bfc613e567
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-11-14 23:04:22 +0330

    قزار گرفتن دکمه زیر متن

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1 className="text-red-600">به پشتیبانی خوش آمدید</h1>
-       <div className="flex items-center justify-center">
-        <ClassButton >
-          ورود
-          </ClassButton>
-       </div>
-
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="text-red-600">به پشتیبانی خوش آمدید</h1>
+          <ClassButton>ورود</ClassButton>
+        </div>
       </main>
     </div>
   );
