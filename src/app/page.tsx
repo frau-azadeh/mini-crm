ed16a26a13ffe1ae5d3d77cb6e090b24c61ec2b1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-11-14 23:05:40 +0330

    فاصله با کامپوننت

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <main>
         <div className="flex items-center justify-center flex-col min-h-screen">
-          <h1 className="text-red-600">به پشتیبانی خوش آمدید</h1>
+          <h1 className="text-red-600 mb-6">به پشتیبانی خوش آمدید</h1>
           <ClassButton>ورود</ClassButton>
         </div>
       </main>
